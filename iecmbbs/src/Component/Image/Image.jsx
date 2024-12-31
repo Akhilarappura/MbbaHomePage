2e29248a344bd2c1d59a2746e42f27ebf0beb863
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-12-31 20:34:07 +0530

    image

--- iecmbbs/src/Component/Image/Image.jsx
+++ iecmbbs/src/Component/Image/Image.jsx
@@ -4,7 +4,7 @@ import './Image.css'
 export default function Image() {
   return (
     <div className='image-first'>
-      <img className='image' src='//iecmbbs.com/wp-content/uploads/2021/06/Main-banner-01-100x50.jpg'  />
+      <img className='image' src='https://www.worldwidecolleges.com/wp-content/uploads/2024/07/Study-MBBS-abroad-indian-students.jpg'  />
       {/* <img className='image' src='https://www.worldwidecolleges.com/wp-content/uploads/classified-listing/2023/04/homebanner2.jpg'/> */}
     </div>
   )
