9b7114a329fcecfe6f752d0e3aa3f8b7b1fef351
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-12-31 20:45:09 +0530

    image.3

--- iecmbbs/src/Component/Nav/Navbar.jsx
+++ iecmbbs/src/Component/Nav/Navbar.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="Navbar">
-      
-      <div className={`nav-items ${isOpen && "open"}`}>
+      <div className="nav-logo"></div>
+      <div className={`nav-items ${isOpen ? "open" : ""}`}>
         <a href="/home">Home</a>
         <a href="/about">About</a>
         <a href="/service">Service</a>
         <a href="/contact">Contact</a>
       </div>
       <div
-        className={`nav-toggle ${isOpen && "open"}`}
+        className={`nav-toggle ${isOpen ? "open" : ""}`}
         onClick={() => setIsOpen(!isOpen)}
       >
         <div className="bar"></div>
