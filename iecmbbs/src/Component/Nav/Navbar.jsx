8e2ec45ea4f63b7c0870646549ce7ba2e62f10f7
Akhilarappura <itsmeakhilarappura@gmail.com>
2024-12-31 20:13:39 +0530

    responsive

--- iecmbbs/src/Component/Nav/Navbar.jsx
+++ iecmbbs/src/Component/Nav/Navbar.jsx
@@ -1,75 +1,24 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import Header from "../Header/Header";
-import New from "../New/New";
-
-export default function Navbar() {
-  const [hoveredItem, setHoveredItem] = useState(null);
-
-  const handleMouseEnter = (item) => {
-    setHoveredItem(item);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredItem(null);
-  };
-
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="nav">
-      <ul className="nav-ul">
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("home")}
-          onMouseLeave={handleMouseLeave}
-        >
-          Home
-          {hoveredItem === "home" && (
-            <div className="hover-content">
-              <New />
-            </div>
-          )}
-        </li>
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("about")}
-          onMouseLeave={handleMouseLeave}
-        >
-          About Us
-          {hoveredItem === "about" && <div className="hover-content"></div>}
-        </li>
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("country")}
-          onMouseLeave={handleMouseLeave}
-        >
-          Country
-          {hoveredItem === "country" && <div className="hover-content"></div>}
-        </li>
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("country")}
-          onMouseLeave={handleMouseLeave}
-        >
-          University
-          {hoveredItem === "country" && <div className="hover-content"></div>}
-        </li>
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("country")}
-          onMouseLeave={handleMouseLeave}
-        >
-          Gallery
-          {hoveredItem === "country" && <div className="hover-content"></div>}
-        </li>
-        <li
-          className="nav-item"
-          onMouseEnter={() => handleMouseEnter("country")}
-          onMouseLeave={handleMouseLeave}
-        >
-          Contact Us
-          {hoveredItem === "country" && <div className="hover-content"></div>}
-        </li>
-      </ul>
+    <div className="Navbar">
+      
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/service">Service</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
     </div>
   );
-}
+};
+
+export default Navbar;
